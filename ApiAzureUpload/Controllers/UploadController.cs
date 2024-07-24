63a98b75fceb1806538f7df3b9ee5cd45009456f
Lucas Santana <lucasmudile@gmail.com>
2024-07-24 22:51:18 +0100

    File upload in Azure

---
+++ ApiAzureUpload/Controllers/UploadController.cs
@@ -0,0 +1,38 @@
+﻿using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace ApiAzureUpload.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> UploadFile([FromBody]string base64)
+        {
+            // Gera um nome randomico para imagem
+            var fileName = Guid.NewGuid().ToString() + ".jpg";
+
+            // Limpa o hash enviado
+            var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(base64, "");
+
+            // Gera um array de Bytes
+            byte[] imageBytes = Convert.FromBase64String(data);
+
+            // Define o BLOB no qual a imagem será armazenada
+            var blobClient = new BlobClient("Connection String","the name of conrainer" , fileName);
+
+            // Envia a imagem
+            using (var stream = new MemoryStream(imageBytes))
+            {
+                blobClient.UploadAsync(stream);
+            }
+            // Retorna a URL da imagem
+            return Ok(blobClient.Uri.AbsoluteUri);
+        }
+
+    }
+}
